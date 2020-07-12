92cf92ea393ad31091d7d9a21fb9cf165a5bc8c3
Frovy <sfrovis@gmail.com>
2020-07-12 05:36:47 +0300

    fix: YOU SPIN ME RIGHT ROUND BABY RIGHT ROUND

--- front/js/earth/earth.js
+++ front/js/earth/earth.js
@@ -49,9 +49,9 @@ function init() {
     const earthMaterial = new THREE.MeshLambertMaterial( { map: earthTexture } );
 	const earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
 	// rotating the earth by some magic numbers so traces will be drawn as they should
-	earthMesh.rotation.x = -0.165;
-	earthMesh.rotation.y = -0.021;
-	earthMesh.rotation.z = 0.425;
+	earthMesh.rotation.x = (23.44 / 2) * (Math.PI/180);
+	earthMesh.rotation.y = 0;
+	earthMesh.rotation.z = (-23.44) * (Math.PI/180);
 	camera.up = new THREE.Vector3(0, 1, 0);
 	camera.position.z = 9;
 	camera.position.y = 5;
