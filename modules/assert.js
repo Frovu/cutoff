15b855b5cdbd5ec6af39348cd6358df7795abbbb
Frovy <sfrovis@gmail.com>
2020-07-13 22:54:43 +0300

    add: forbid too hard calculations

--- modules/assert.js
+++ modules/assert.js
@@ -3,28 +3,34 @@ const ranges = require('../'+config.valueRanges);
 
 module.exports = function(ini) {
 	// check value ranges
-	for(param of Object.keys(ranges)) {
+	for(const param of Object.keys(ranges)) {
 		if(typeof ini[param] === 'undefined') { // parameter was not specified
 			// fail if param is needed for every model or for current model
-			if((typeof ranges[param]["for"] === 'undefined') ||
-			 		(ranges[param]["for"].includes(ini["model"])) )
+			if((typeof ranges[param].for === 'undefined') ||
+			 		(ranges[param].for.includes(ini.model)) )
 				return false;
 		}
-		if(typeof ranges[param]["range"] !== 'undefined') {
-			if(!ranges[param]["range"].includes(ini[param]))
+		if(typeof ranges[param].range !== 'undefined') {
+			if(!ranges[param].range.includes(ini[param]))
 				return false;
 		}
 		else
-			if(ini[param] > ranges[param]["max"] || ini[param] < ranges[param]["min"])
+			if(ini[param] > ranges[param].max || ini[param] < ranges[param].min)
 				return false;
 		// round of needed
-		if(ranges[param]["int"] === true) {
-			ini[param] = Math.trunc(ini[param]);
+		if(ranges[param].int === true) {
+			ini[param] = String(Math.trunc(ini[param]));
 		}
 	}
 	// additional checks
-	if(ini["lower"] >= ini["upper"])
+	if(ini.lower >= ini.upper)
 		return false;
+	// forbid calculations that are about to take really much time
+	if(['96', '01'].includes(ini.model) &&
+	(parseFloat(ini.upper) - parseFloat(ini.lower)) /  parseFloat(ini.step)
+	 								> (ini.model==='01'?4000:8000))
+		return false;
+
 	// all checks passed
 	return true;
 };
